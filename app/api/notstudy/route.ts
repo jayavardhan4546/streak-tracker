0935de0f7330efd61818596d8c31b03672489ea3
jayavardhan4546 <gjayavardhan17@gmail.com>
2026-03-14 01:03:01 +0530

    fix vercel import path

--- app/api/notstudy/route.ts
+++ app/api/notstudy/route.ts
@@ -1,21 +1,21 @@
 import { NextResponse } from "next/server"
-import { missedDates } from "@/lib/data"
+import { missedDates } from "../../../lib/data"
 
-export async function POST(req:Request){
+export async function POST(req: Request) {
 
   const body = await req.json()
   const date = body.date
 
-  if(missedDates.includes(date)){
+  if (missedDates.includes(date)) {
     return NextResponse.json({
-      message:"Already marked not studied"
+      message: "Already marked not studied"
     })
   }
 
   missedDates.push(date)
 
   return NextResponse.json({
-    message:"Marked as not studied"
+    message: "Marked as not studied"
   })
 
 }
